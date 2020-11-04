93c817aa4b1682b62a3d85690251efa1b085579f
RodrigoEC <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2020-11-04 11:29:49 -0300

    Refatoraçao: Nao salva mais o arquivo anon.txt

--- anon.py
+++ anon.py
@@ -3,22 +3,19 @@ import requests
 URL = 'http://lad.ufcg.edu.br/loac/uploads/OAC/anon.txt'
 response = requests.get(URL)
 
-with open('anon.txt', 'w') as file:
-    file.write(response.text)
-
-with open('anon.txt', 'r') as file:
-    dict_apelidos = {}
-    for linha in file.readlines():
-        [identificador, _, centavos, *_] = linha.split() 
+dict_apelidos = {}
+for linha in response.text.split('\n'):
+    if len(linha) > 3:
+        [identificador, data, centavos, *_] = linha.split()
         
         if identificador not in dict_apelidos:
             dict_apelidos[identificador] = []
 
         dict_apelidos[identificador].append(int(centavos))
 
-    apelido = input('Coloque o seu identificador: ')
+apelido = input('Coloque o seu identificador: ')
 
-    if apelido not in dict_apelidos.keys():
-        print(f'\nO apelido {apelido} não existe.\nVerifique se você escreveu de forma correta\n')
-    else:
-        print(f'\nVocê tem: --{sum(dict_apelidos[apelido])}-- centavos.')
+if apelido not in dict_apelidos.keys():
+    print(f'\nO apelido {apelido} não existe.\nVerifique se você escreveu de forma correta\n')
+else:
+    print(f'\nVocê tem: --{sum(dict_apelidos[apelido])}-- centavos.')
